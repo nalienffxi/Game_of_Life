dce91db5f431362550c429dce04dfdc7f14cc7c8
nalienffxi <131621894+nalienffxi@users.noreply.github.com>
2023-04-24 04:51:30 +0200

    Delete game_of_life directory

--- game_of_life/hasher.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def compute_hash(quadtree):
-    serialized_quadtree = quadtree.serialize()
-    hasher = hashlib.sha256()
-    hasher.update(serialized_quadtree.encode('utf-8'))
-    return hasher.hexdigest()
-
